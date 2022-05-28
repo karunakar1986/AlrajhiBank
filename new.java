f78420ce01c7e6e6be22b753c49bd583d1f033f5
karuna <karunakar12naga@gmail.com>
2022-05-28 05:54:58 +0000

    new.java added CIMB

---
+++ new.java
@@ -0,0 +1,13 @@
+package javaproject;
+
+public class first {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("my first java");
+		System.out.println("Second line java");
+		
+
+	}
+
+}
